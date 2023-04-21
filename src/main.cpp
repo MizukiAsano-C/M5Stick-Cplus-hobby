9972c587e845c64a5602d73f18e65dedf87f4de1
MizukiAsano <cbc.nml739@gmail.com>
2023-04-21 14:20:43 +0900

    ボタンを押すたびにHello Worldを表示し、いっぱいになったら消すプログラム完成!

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,46 @@
 void setup()
 {
   M5.begin();
-  M5.Lcd.print("Hello world");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setRotation(3);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("Hello world");
   Serial.begin(115200);
   pinMode(MAIN_BUTTON, INPUT);
 }
 
 void loop()
 {
+  static bool button_pressed = true;
+  static bool printed = false;
+  static int count = 0;
+
+  bool now_button;
+
+  now_button = !digitalRead(MAIN_BUTTON);
+
+  if ((button_pressed == false) && (now_button == true))
+  {
+    button_pressed = true;
+  }
+  else if (now_button == false)
+  {
+    button_pressed = false;
+    printed = false;
+  }
+
   M5.update();
-  if (digitalRead(MAIN_BUTTON))
+  if (button_pressed && !printed)
   {
-    M5.Lcd.fillScreen(TFT_BLACK);
-    M5.Lcd.print("Hello world");
+    M5.Lcd.println("Hello world");
+    count++;
+    printed = true;
   }
-  else
+
+  if (count > 6)
   {
-    M5.Lcd.fillScreen(BLUE);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    count = 0;
   }
-  delay(500);
 }
