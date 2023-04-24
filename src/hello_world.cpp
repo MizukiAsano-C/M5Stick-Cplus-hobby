d3bd32215d45ce60cd5bfc961dc84501a8997897
MizukiAsano <cbc.nml739@gmail.com>
2023-04-24 15:09:59 +0900

    バッテリー確認用のファームを追加。

---
+++ src/hello_world.cpp
@@ -0,0 +1,50 @@
+#include <M5StickCPlus.h>
+
+#define MAIN_BUTTON 37
+
+void setup_hw()
+{
+    M5.begin();
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setRotation(3);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Hello world");
+    Serial.begin(115200);
+    pinMode(MAIN_BUTTON, INPUT);
+}
+
+void loop_hw()
+{
+    static bool button_pressed = true;
+    static bool printed = false;
+    static int count = 0;
+
+    bool now_button;
+
+    now_button = !digitalRead(MAIN_BUTTON);
+
+    if ((button_pressed == false) && (now_button == true))
+    {
+        button_pressed = true;
+    }
+    else if (now_button == false)
+    {
+        button_pressed = false;
+        printed = false;
+    }
+
+    M5.update();
+    if (button_pressed && !printed)
+    {
+        M5.Lcd.println("Hello world");
+        count++;
+        printed = true;
+    }
+
+    if (count > 6)
+    {
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setCursor(0, 0);
+        count = 0;
+    }
+}
